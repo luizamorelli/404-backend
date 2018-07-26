210929821c83d6458a2c6965ae37056f059f5f83
Luiza Morelli <luizam22@hotmail.com>
2018-07-26 09:24:52 -0300

    Adicionar primeiro exemplo de entrada e saida

---
+++ node.js
@@ -0,0 +1,14 @@
+console.log(typeof(process.argv[2]))
+
+if(process.argv.length === 3 && !isNaN(Number(process.argv[2]) === 'number')){
+    let base = process.argv[2];
+
+    for (let i = 1; i<=10; i++){
+        console.log(base*i);
+    }
+}
+else{
+    console.log("Utilização");
+    console.log("node tabuada.js <numero>")
+}
+
